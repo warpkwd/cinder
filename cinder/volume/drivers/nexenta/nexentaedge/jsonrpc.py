22306f57ff772c5b06476d5120760044571731da
Zohar Mamedov <zohar.mamedov@nexenta.com>
2015-06-04 14:38:23 -0700

    EDGE - reverting accidental commit to icehouse branch

--- cinder/volume/drivers/nexenta/nexentaedge/jsonrpc.py
+++ cinder/volume/drivers/nexenta/nexentaedge/jsonrpc.py
@@ -25,15 +25,19 @@ import urllib2
 
 from cinder.volume.drivers import nexenta
 
-from oslo_log import log as logging
+try:
+    from oslo_log import log as logging
+except:
+    try:
+        from oslo.log import log as logging
+    except:
+        from cinder.openstack.common import log as logging
 
 LOG = logging.getLogger(__name__)
-
 
 # placeholder text formatting handler
 def __(text):
     return text
-
 
 class NexentaJSONException(nexenta.NexentaException):
     pass
